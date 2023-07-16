f227c9971e10f776ba1b361643a4880b07ced42d
Sathishkumar-Ramachandran <sathish.spacy2001@gmail.com>
2023-07-16 18:07:59 +0530

    DB Added

--- GoogleSchema/src/config/Dbconnection.js
+++ GoogleSchema/src/config/Dbconnection.js
@@ -2,6 +2,8 @@ const moongoose= require('mongoose');
 moongoose.set('strictQuery', true);
 
 
+const DB = "mongodb+srv://teamproject:Sathish123@cluster0.wqp3wtc.mongodb.net/?retryWrites=true&w=majority"
+
 const connectDB= async()=>{
     const mongoOptions = {
         useNewUrlParser: true,
@@ -9,7 +11,7 @@ const connectDB= async()=>{
         connectTimeoutMS: 30000, // Set connection timeout to 30 seconds
       };
     try{
-     const con=await moongoose.connect("mongodb://127.0.0.1:27017/signup")
+     const con=await moongoose.connect(DB)
      console.log(`connected to the MongoDB Database ${con.connection.name}`);
     }
     catch(err){
